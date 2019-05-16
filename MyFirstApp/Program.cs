d2621552eed57ba85790aa59c53496d957cfc7f4
gstas <gstas07@gmail.com>
2019-05-16 14:27:29 +0300

    new message

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -10,9 +10,9 @@ namespace MyFirstApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("new app - Hello world2!");
+            Console.WriteLine("new app - Hello world!");
             string readLine = Console.ReadLine();
-            Console.WriteLine(readLine);
+            Console.WriteLine("Вы ввели:" + readLine);
             Console.ReadLine();
         }
     }
